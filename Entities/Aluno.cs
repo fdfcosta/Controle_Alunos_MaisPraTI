4a1e7b01c712d96ab8e15f62360001b09a894299
fdfcosta <devcode.fabricio@gmail.com>
2022-05-28 09:49:36 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Alunos.Entities
+{
+    public class Aluno
+    {
+        public string Nome{ get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+
+        public Aluno() { }
+
+        public Aluno(string Nome, int Idade, string Email)
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+            this.Email = Email;
+        }
+
+        public void AlteraNome(string NovoNome)
+        {
+            Nome = NovoNome;
+        }
+
+        public void AlteraIdade(int NovaIdade)
+        {
+            Idade = NovaIdade;
+        }
+    
+        public void AlteraEmail(string NovoEmail)
+        {
+            Email = NovoEmail;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} - Idade: {Idade} - E-mail: {Email}";
+
+        }
+    }
+}
---
+++ Entities/AlunoValidacao.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace Alunos.Entities
+{
+    public class AlunoValidacao : AbstractValidator<Aluno>
+    {
+        
+
+        bool ValidaEmail(string Email)
+        {
+
+            bool RegexEmailTeste = Regex.IsMatch(Email, @"/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+
+            bool EmailContem = Email.EndsWith(".com") || Email.EndsWith(".net") || Email.EndsWith(".br");
+
+            //separa o email usando @ como parâmetro
+            var SplitEmail = Email.Split('@');
+
+            //todo endereço de e-mail consiste em três elementos: parte local, símbolo '@' e nome do domínio
+            //a posicao 0 do array será a parte local do email, a posicao 1 será o domínio
+            //a parte local do email precisa conter no mínimo 3 caracteres
+            bool ParteLocalValida = SplitEmail[0].Length >= 3;
+
+            return RegexEmailTeste && EmailContem && ParteLocalValida;
+
+        }
+
+
+         
+
+        public AlunoValidacao()
+        {
+            RuleFor(x => x.Nome).Length(3, 70).Matches(@"^[\p{L} \.']+$");
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Idade).GreaterThan(1).NotEmpty().NotNull();  
+        }
+    }
+}
