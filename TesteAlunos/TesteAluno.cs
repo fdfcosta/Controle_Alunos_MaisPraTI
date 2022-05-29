69889f7ba350107f53a402bfeb80ab5c90aadcdb
fdfcosta <devcode.fabricio@gmail.com>
2022-05-29 19:51:34 -0300

    Corrigindo erro nos arquivos de teste.

---
+++ TesteAlunos/TesteAluno.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Alunos.Entities;
+
+namespace TesteAlunos
+{
+    public class TesteAluno
+    {
+        [Fact]
+        public void TesteAlteraNome()
+        {
+            //testando side-effect causado pelo método AlteraNome() utilizando estrutura AAA
+
+            //Arrange
+            Aluno Aluno = new();
+            string Nome = "Fabrício";
+
+
+            //Act
+            Aluno.AlteraNome(Nome);
+
+            //Assert
+            Assert.Equal(Nome, Aluno.Nome);
+
+        }
+
+        [Fact]
+        public void TesteAlteraIdade()
+        {
+            Aluno Aluno = new();
+            int Idade = 20;
+
+            Aluno.AlteraIdade(Idade);
+
+            Assert.Equal(Idade, Aluno.Idade);
+        }
+
+        [Fact]
+        public void TesteAlteraEmail()
+        {
+            Aluno aluno = new();
+            string Email = "devcode.fabricio@gmail.com";
+
+            aluno.AlteraEmail(Email);
+
+            Assert.Equal(Email, aluno.Email);
+
+        }
+    }
+
+
+}
---
+++ TesteAlunos/TesteControleAlunos.cs
@@ -0,0 +1,128 @@
+﻿using Xunit;
+using Alunos.Entities;
+
+namespace TesteAlunos
+{
+    public class ControleAlunosTeste
+    {
+        [Fact]
+        public void TesteValidaNome()
+        {
+            //arrange
+            ControleAlunos c = new();
+            string Nome = "Fabrício Del Frari da Costa";
+
+            //act
+            bool resultado = c.ValidaNome(Nome);
+            bool esperado = true;
+
+            //Assett
+            Assert.Equal(esperado, resultado);
+
+        }
+
+        [Fact]
+        public void TesteRetiraAcentosNome()
+        {
+            //arrange
+            ControleAlunos c = new();
+            string letraComAcento = "áàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ";
+
+            //act
+            string esperado = "aaaaeeeiiooooucnAAAAEEIIOOOOUCN";
+            string resultado = c.RetiraAcentosNome(letraComAcento);
+
+            //Assert
+            Assert.Equal(esperado, resultado);
+
+        }
+
+        [Fact]
+        public void TesteValidaIdade()
+        {
+            //arrange
+            ControleAlunos c = new();
+            int IdadeValida = 25;
+            int IdadeInvalida = 200;
+
+            //act
+            var resultadoValido = c.ValidaIdade(IdadeValida);
+            var resultadoInvalido = c.ValidaIdade(IdadeInvalida);
+
+            //Assert
+            bool esperadoValido = true;
+            bool esperadoInvalido = false;
+
+            Assert.Equal(esperadoValido, resultadoValido);
+            Assert.Equal(esperadoInvalido, resultadoInvalido);
+
+        }
+
+        [Fact]
+        public void TesteValidaEmail()
+        {
+            //arrange
+            ControleAlunos c = new();
+            string emailValido = "devcode.fabricio@gmail.com";
+            string parteLocalInvalida = "ds@gmail.com";
+            string dominioInvalido = "fabricio@.net";
+            string dominioDeTopoInvalido = "fabricio@gmail.açslçldaçld";
+
+            //act
+            var resultadoValido = c.ValidaEmail(emailValido);
+            var esperadoValido = true;
+            var resultadoInvalido = c.ValidaEmail(parteLocalInvalida) == true && c.ValidaEmail(dominioInvalido) == true && c.ValidaEmail(dominioDeTopoInvalido) == true;
+            var esperadoInvalido = false;
+
+            //assert
+            Assert.Equal(esperadoValido, resultadoValido);
+            Assert.Equal(esperadoInvalido, resultadoInvalido);
+
+        }
+
+        [Fact]
+        public void TesteAlunoJaExiste()
+        {
+            //arrange
+            ControleAlunos c = new();
+            c.ListaAlunos.Clear();
+            c.ListaAlunos.Add(new Aluno()
+            {
+                Nome = "Fabrício Del Frari da Costa",
+                Idade = 25,
+                Email = "devcode.fabricio@gmail.com"
+            });
+            string Nome = c.ListaAlunos[0].Nome;
+            int Idade = c.ListaAlunos[0].Idade;
+
+            //act
+            var esperado = true;
+            var resultado = c.AlunoJaExiste(Nome, Idade);
+
+            //assert
+            Assert.Equal(esperado, resultado);
+        }
+
+        [Fact]
+        public void TesteExisteAlunos()
+        {
+            //arrange
+            ControleAlunos c = new();
+            c.ListaAlunos.Clear();
+            c.ListaAlunos.Add(new Aluno()
+            {
+                Nome = "Fabrício Del Frari",
+                Idade = 25,
+                Email = "devcode.fabricio@gmail.com"
+            });
+            var existeAlunos = c.ExisteAlunos();
+
+            //act
+            var esperado = true;
+            var resultado = existeAlunos;
+
+            //assert
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
